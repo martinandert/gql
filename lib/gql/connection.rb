0d6ce173bdd84190f9a18f9b9f356c976219356e
martinandert <mandert@gmail.com>
2015-03-05 16:54:31 +0100

    move things around

--- lib/gql/connection.rb
+++ lib/gql/connection.rb
@@ -1,18 +1,34 @@
 require 'active_support/core_ext/class/attribute'
 
 module GQL
-  class Connection < Node
-    class_attribute :node_class, instance_accessor: false, instance_predicate: false
+  module Fields
+    class Connection < Field
+      class_attribute :connection_class, instance_accessor: false, instance_predicate: false
+      self.connection_class = GQL::Connection
 
-    class << self
-      def build_class(node_class)
-        node_class ||= self.node_class
+      class << self
+        def build_class(id, method, options = {})
+          connection_class = options[:connection_class] || self.connection_class
 
-        Class.new(self).tap do |connection_class|
-          connection_class.array :edges, node_class: node_class do
-            target
+          if connection_class.nil?
+            raise Errors::UndefinedNodeClass.new(self, 'connection')
+          end
+
+          unless connection_class <= GQL::Connection
+            raise Errors::InvalidNodeClass.new(connection_class, GQL::Connection)
+          end
+
+          Class.new(self).tap do |field_class|
+            field_class.id = id.to_s
+            field_class.method = method
+            field_class.connection_class = connection_class.build_class(options[:node_class])
           end
         end
+      end
+
+      def value
+        connection = self.class.connection_class.new(ast_node, target, variables, context)
+        connection.value
       end
     end
   end
