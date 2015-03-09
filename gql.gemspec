e70e9bfa6b7378ab4105358d1e19006c006455e0
martinandert <mandert@gmail.com>
2015-03-09 22:45:28 +0100

    config tests, part 2

--- gql.gemspec
+++ gql.gemspec
@@ -23,6 +23,7 @@ Gem::Specification.new do |spec|
   spec.add_development_dependency 'rake', '~> 10.0'
   spec.add_development_dependency 'rexical', '~> 1.0'
   spec.add_development_dependency 'racc', '~> 1.4'
+  spec.add_development_dependency 'codeclimate-test-reporter', '~> 0.4'
 
   spec.add_dependency 'activesupport', '~> 4.0'
   spec.add_dependency 'multi_json', '~> 1.0'
--- test/cases/gql_test.rb
+++ test/cases/gql_test.rb
@@ -24,12 +24,12 @@ class GQLTest < GQL::TestCase
 
   test "can set the root node class to a valid class" do
     begin
-      prev_root = GQL.root_node_class
+      prev = GQL.root_node_class
 
       assert_nothing_raised { GQL.root_node_class = MyGQLNode }
       assert_equal MyGQLNode, GQL.root_node_class
     ensure
-      GQL.root_node_class = prev_root
+      GQL.root_node_class = prev
     end
   end
 
@@ -39,18 +39,51 @@ class GQLTest < GQL::TestCase
     end
   end
 
+  test "has a root target proc set by default" do
+    assert_instance_of Proc, GQL.root_target_proc
+  end
+
+  test "can set the root target proc" do
+    begin
+      prev = GQL.root_target_proc
+      proc = -> { }
+
+      assert_nothing_raised { GQL.root_target_proc = proc }
+      assert_equal proc, GQL.root_target_proc
+    ensure
+      GQL.root_target_proc = prev
+    end
+  end
+
+  test "has basic field types set by default" do
+    assert_instance_of Hash, GQL.field_types
+    assert_not_empty GQL.field_types
+  end
+
+  test "can set the field types" do
+    begin
+      prev = GQL.field_types
+      hash = { foo: 'bar' }
+
+      assert_nothing_raised { GQL.field_types = hash }
+      assert_equal hash, GQL.field_types
+    ensure
+      GQL.field_types = prev
+    end
+  end
+
   test "uses GQL::Node as the default list class" do
     assert_equal GQL::Node, GQL.default_list_class
   end
 
   test "can set the default list class to a valid class" do
     begin
-      prev_list = GQL.root_node_class
+      prev = GQL.root_node_class
 
       assert_nothing_raised { GQL.default_list_class = MyGQLNode }
       assert_equal MyGQLNode, GQL.default_list_class
     ensure
-      GQL.default_list_class = prev_list
+      GQL.default_list_class = prev
     end
   end
 
@@ -60,5 +93,35 @@ class GQLTest < GQL::TestCase
     end
   end
 
-  # to be continued...
+  test "has a default field proc set by default" do
+    assert_instance_of Proc, GQL.default_field_proc
+  end
+
+  test "can set the default field proc" do
+    begin
+      prev = GQL.default_field_proc
+      proc = -> { }
+
+      assert_nothing_raised { GQL.default_field_proc = proc }
+      assert_equal proc, GQL.default_field_proc
+    ensure
+      GQL.default_field_proc = prev
+    end
+  end
+
+  test "has a default call proc set by default" do
+    assert_instance_of Proc, GQL.default_call_proc
+  end
+
+  test "can set the default call proc" do
+    begin
+      prev = GQL.default_call_proc
+      proc = -> { }
+
+      assert_nothing_raised { GQL.default_call_proc = proc }
+      assert_equal proc, GQL.default_call_proc
+    ensure
+      GQL.default_call_proc = prev
+    end
+  end
 end
