7f8c00d4676549db942e61081a8b9f771ec6091a
martinandert <mandert@gmail.com>
2015-03-10 01:14:44 +0100

    add missing test

--- test/cases/schema_test.rb
+++ test/cases/schema_test.rb
@@ -16,8 +16,11 @@ end
 class AstField < Struct.new(:id, :alias_id, :call, :fields)
 end
 
+class ClassWithProc < Struct.new(:proc)
+end
+
 class SchemaTest < GQL::TestCase
-  test "call parameters" do
+  test "call class parameters" do
     ast_node = AstField.new(nil, nil, nil, [
       AstField.new(:parameters, nil, nil, [
         AstField.new(:id, nil, nil, nil),
@@ -26,6 +29,21 @@ class SchemaTest < GQL::TestCase
     ])
 
     schema_call = GQL::Schema::Call.new(ast_node, NodeForSchemaCall, {}, {})
+
+    expected = [{ id: 'a', mode: 'required' }, { id: 'b', mode: 'rest' }, { id: 'c', mode: 'block' }]
+
+    assert_equal expected, schema_call.value[:parameters]
+  end
+
+  test "call proc parameters" do
+    ast_node = AstField.new(nil, nil, nil, [
+      AstField.new(:parameters, nil, nil, [
+        AstField.new(:id, nil, nil, nil),
+        AstField.new(:mode, nil, nil, nil)
+      ])
+    ])
+
+    schema_call = GQL::Schema::Call.new(ast_node, ClassWithProc.new(-> (a, *b, &c) { }), {}, {})
 
     expected = [{ id: 'a', mode: 'required' }, { id: 'b', mode: 'rest' }, { id: 'c', mode: 'block' }]
 
