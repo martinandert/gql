c282b161e8c544147ccc9c27bac21fe8cd72bdbc
martinandert <mandert@gmail.com>
2015-03-14 00:07:11 +0100

    refactor has_calls

--- lib/gql/has_calls.rb
+++ lib/gql/has_calls.rb
@@ -18,29 +18,19 @@ module GQL
       def add_call(id, *args, &block)
         remove_call id
 
-        id = id.to_sym
-        options = args.extract_options!
+        id            = id.to_sym
+        options       = args.extract_options!
+        call_spec     = args.shift || block || proc_for_call(id)
+        result_spec   = options[:returns] || call_spec.try(:result_class)
+        result_class  = result_class_from_spec(result_spec)
 
-        call_spec = args.shift || block || proc_for_call(id)
-        result_spec = options[:returns] || call_spec.try(:result_class)
-        result_class = result_class_from_spec(result_spec)
+        build_call_class(call_spec, id, result_class).tap do |call_class|
+          const_set const_name_for_call(id), call_class
 
-        result_class = Registry.fetch(result_class) if result_class
-
-        call_class = call_class_from_spec(call_spec)
-        call_class.id = id
-        call_class.result_class = result_class
-
-        if result_class && result_class.name.nil?
-          call_class.const_set :Result, result_class
+          [self, *descendants].each do |c|
+            c.calls = c.calls.merge(id => call_class)
+          end
         end
-
-        const_set const_name_for_call(id), call_class
-        self.calls = calls.merge(id => call_class)
-
-        descendants.each { |c| c.calls[id] = call_class }
-
-        call_class
       end
 
       alias :call :add_call
@@ -62,6 +52,31 @@ module GQL
       end
 
       private
+        def build_call_class(spec, id, result_class)
+          call_class_from_spec(spec).tap do |call_class|
+            call_class.id = id
+            call_class.result_class = result_class
+
+            if result_class && result_class.name.nil?
+              call_class.const_set :Result, result_class
+            end
+          end
+        end
+
+        def call_class_from_spec(spec)
+          return Class.new(spec) unless spec.is_a?(Proc)
+
+          Class.new(Call).tap do |call_class|
+            call_class.class_eval do
+              self.proc = spec
+
+              def execute(*args)
+                instance_exec(*args, &self.class.proc)
+              end
+            end
+          end
+        end
+
         def const_name_for_call(id)
           :"#{id.to_s.camelize}Call"
         end
@@ -71,14 +86,17 @@ module GQL
         end
 
         def result_class_from_spec(spec)
-          case spec
-          when ::Array
-            result_class_from_connection_spec spec.dup
-          when ::Hash
-            result_class_from_mapping_spec spec.dup
-          else
-            spec
-          end
+          result_class =
+            case spec
+            when ::Array
+              result_class_from_connection_spec spec.dup
+            when ::Hash
+              result_class_from_mapping_spec spec.dup
+            else
+              spec
+            end
+
+          result_class && Registry.fetch(result_class)
         end
 
         def result_class_from_connection_spec(spec)
@@ -96,20 +114,6 @@ module GQL
 
         def result_class_from_mapping_spec(spec)
           Object.build_class :result, nil, class: spec
-        end
-
-        def call_class_from_spec(spec)
-          return Class.new(spec) unless spec.is_a?(Proc)
-
-          Class.new(Call).tap do |call_class|
-            call_class.class_eval do
-              self.proc = spec
-
-              def execute(*args)
-                instance_exec(*args, &self.class.proc)
-              end
-            end
-          end
         end
     end
 
