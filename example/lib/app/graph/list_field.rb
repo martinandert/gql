d8b34531e71004f6c4899dc5a9d1b44b7aefa2cd
martinandert <mandert@gmail.com>
2015-03-14 20:40:38 +0100

    enable now working example feature

--- example/lib/app/graph/list_field.rb
+++ example/lib/app/graph/list_field.rb
@@ -1,14 +1,14 @@
 module App
   module Graph
     class ListField < GQL::Field
-      # MODEL_TO_FIELD_MAPPING = {
-      #   Models::Person      => PersonField,
-      #   Models::Band        => BandField,
-      #   Models::Album       => AlbumField,
-      #   Models::Song        => SongField,
-      #   Models::Role        => RoleField,
-      #   Models::Membership  => MembershipField
-      # }.freeze
+      MODEL_TO_FIELD_MAPPING = {
+        Models::Person      => 'App::Graph::PersonField',
+        Models::Band        => 'App::Graph::BandField',
+        Models::Album       => 'App::Graph::AlbumField',
+        Models::Song        => 'App::Graph::SongField',
+        Models::Role        => 'App::Graph::RoleField',
+        Models::Membership  => 'App::Graph::MembershipField'
+      }.freeze
 
       number :count
       boolean :any, -> { target.any? }
@@ -17,8 +17,8 @@ module App
       call :take, -> size { target.limit(size) }
 
       # proc given here b/c we want no arguments (-> just a single record) and raise! if not found
-      # call :first, -> { target.first! }, returns: MODEL_TO_FIELD_MAPPING
-      # call :last, ->  { target.last!  }, returns: MODEL_TO_FIELD_MAPPING
+      call :first, -> { target.first! }, returns: MODEL_TO_FIELD_MAPPING
+      call :last, ->  { target.last!  }, returns: MODEL_TO_FIELD_MAPPING
     end
   end
 end
